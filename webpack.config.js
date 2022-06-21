b68015bc6e1f8de8a05396af0baf8c4c6678b7c2
HAN-homePC <thepub03@gmail.com>
2022-06-21 15:56:12 +0900

    webpack dev-server and httpwebpackplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const webpack = require('webpack');
-const banner = require("./banner.js")
+const banner = require("./banner.js");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: "development",
@@ -31,6 +32,12 @@ module.exports = {
       PRODUCTION: JSON.stringify(false),
       MAX_COUNT: JSON.stringify(999),
       "api.domain": JSON.stringify("http://dev.api.domain.com"),
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // 템플릿 경로를 지정
+      templateParameters: { // 템플릿에 주입할 파라매터 변수 지정
+        env: process.env.NODE_ENV === 'development' ? '(개발용)' : '',
+      },
     })
   ],
 }
